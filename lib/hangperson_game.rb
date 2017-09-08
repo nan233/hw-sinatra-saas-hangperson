e5c7d5e65cc188a32b62bc78f1bfc81dd77b2b63
Nan <jnnancy233@berkeley.edu>
2017-09-08 20:49:24 +0000

    Hangman implementation

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -10,8 +10,51 @@ class HangpersonGame
   
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
+  end
+  
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
+  
+  def guess(attempt)
+    if /[a-z]/i.match(attempt)==nil
+      raise ArgumentError
+    else
+      attempt = attempt.downcase
+      if @guesses.include? attempt or @wrong_guesses.include? attempt
+        false
+      elsif @word.include? attempt
+        @guesses += attempt
+        true
+      else
+        @wrong_guesses += attempt
+        true
+      end
+    end
+  end
+  
+  def word_with_guesses
+    display = '-'*@word.length
+    for i in 0...@word.length do
+      if @guesses.include? @word[i]
+        display[i] = @word[i]
+      end
+      i+=1
+    end
+    display
   end
 
+  def check_win_or_lose
+    if @word==self.word_with_guesses
+      :win
+    elsif @wrong_guesses.length==7
+      :lose
+    else
+      :play
+    end
+  end
   # You can test it by running $ bundle exec irb -I. -r app.rb
   # And then in the irb: irb(main):001:0> HangpersonGame.get_random_word
   #  => "cooking"   <-- some random word
