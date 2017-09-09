2c39022fbe428d1ac36431ee8a0ef44e4c866bd8
Nan <jnnancy233@berkeley.edu>
2017-09-09 04:32:20 +0000

    win/lose

--- app.rb
+++ app.rb
@@ -60,20 +60,26 @@ class HangpersonApp < Sinatra::Base
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     if @game.check_win_or_lose==:win
-      erb :win
+      redirect '/win'
     elsif @game.check_win_or_lose==:lose
-      erb :lose
+      redirect '/lose'
     else
       erb :show
     end
   end
   
   get '/win' do
-    redirect '/show' # You may change/remove this line
+    if @game.check_win_or_lose==:play
+      redirect '/show'
+    end
+    erb :win # You may change/remove this line
   end
   
   get '/lose' do
-    redirect '/show' # You may change/remove this line
+    if @game.check_win_or_lose==:play
+      redirect '/show'
+    end
+    erb :lose # You may change/remove this line
   end
   
 end
